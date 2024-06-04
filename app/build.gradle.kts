985c60c8598bfa3cdd640339e9599acfa1eb88e0
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-06-04 12:05:22 +0200

    Fixing Camera problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,17 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.coil.compose)
+}
+kapt {
+    correctErrorTypes = true
 }
--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.example.cameraapp
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.rememberImagePainter
 import com.example.cameraapp.ui.theme.CameraAppTheme
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +45,83 @@ class MainActivity : ComponentActivity() {
         setContent {
             CameraAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   Column(modifier = Modifier
+                       .fillMaxSize()
+                       .padding(innerPadding)){
+                       ImageCaptureFromCamera()
+                   }
                 }
             }
         }
     }
-}
 
+
+}
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ImageCaptureFromCamera() {
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        context.packageName + ".provider", file
     )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri?>(Uri.EMPTY)
+    }
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()){
+        capturedImageUri = uri
+    }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        }else{
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+        
+    }
+    Column(modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Button(
+            onClick = {
+                val permissionCheckResult = ContextCompat.checkSelfPermission(context, Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)
+                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED){
+                    cameraLauncher.launch(uri)
+                }
+                else{
+                    permissionLauncher.launch(Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)
+                }
+            }
+        ){
+            Text(text = "Capture Image")
+        }
+    }
+    if (capturedImageUri?.path?.isNotEmpty() == true){
+        Image(
+            modifier = Modifier.padding(16.dp, 8.dp),
+            painter = rememberImagePainter(capturedImageUri),
+            contentDescription = null)
+    }else{
+        Image(
+            modifier = Modifier.padding(16.dp, 8.dp),
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = null)
+    }
 }
 
+fun Context.createImageFile():File{
+    val timeStamp = SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(Date())
+    val imageFileName = "JPEG_"+timeStamp + "_"
+    val image = File.createTempFile(imageFileName, ".jpg", externalCacheDir)
+        return image
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CameraAppTheme {
-        Greeting("Android")
+
     }
 }
